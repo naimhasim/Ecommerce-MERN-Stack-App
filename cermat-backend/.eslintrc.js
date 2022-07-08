36bece24c0eee6d074b28c8af48253310a069033
Naim Hasim <ainadanaim@gmail.com>
2022-07-08 16:44:45 +0800

    initial commit

---
+++ cermat-backend/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react-hooks/recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {},
+}
